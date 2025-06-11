d4e1d855ce11d2d4ca137ed9e8e66ce140282e2d
fluque <ing.fernando.luque@gmail.com>
2025-06-11 16:13:08 -0700

    [feature]. Add new files

---
+++ content.py
@@ -0,0 +1,19 @@
+'''
+Clone = Copying the repo locally
+Commit = Save ("checkpoint") changes on our current branch
+Stage = Prepare changes for a commit
+Stash = Save changes for later
+Push = Send local commits to remote repo
+Pull = Fetch remote commits to local repo
+Merge = Joining two different branch
+Rebase = Moving one branch on top of another branch
+Cherry Picking = Duplicating (copying) one commit from one branch to another
+Pull Request (PR) = Request to merge branch
+PR Merge = Merge all commits
+PR Squash and Merge = Squash all commits into ONE and then merge ONE commit
+Protecting Branches
+CODEOWNERS
+GitIgnore
+ReadMe
+Releases, Tags, Versioning
+'''
